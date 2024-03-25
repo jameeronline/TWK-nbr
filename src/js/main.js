c05df6ca9f6f96cb3beffe591a009d941ddb369c
Jamal Mohamed Ameer <jamalbe2004@gmail.com>
2024-03-25 16:52:08 +0300

    feature: detailes page added

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,8 @@
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+
+//Initialize the Copy to clipboard
+import ClipboardJS from "clipboard";
+
+new ClipboardJS(".js-copy-clipboard");
