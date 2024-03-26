fbed5479d7feb82a3d850cbdabcbd5920ad4df87
Jamal Mohamed Ameer <jamalbe2004@gmail.com>
2024-03-26 12:35:02 +0300

    update: pages are linked

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ export default {
   root: resolve(__dirname, "src"),
   build: {
     outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: "./src/index.html",
+        details: "./src/details.html",
+      },
+    },
   },
   server: {
     port: 8080,
